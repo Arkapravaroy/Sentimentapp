0f3d51c354020aa48f6ff8bc2cd285c9a85a8455
Arkapravaroy <39229161+Arkapravaroy@users.noreply.github.com>
2020-06-24 11:08:05 +0530

    Update index.js

--- toxicity/index.js
+++ toxicity/index.js
@@ -58,7 +58,7 @@ const addPredictions = (predictions) => {
 // tf.loadLayersModel('model/model.json')
 //toxicity.load()
 const predict = async () => {
-    model = await tf.loadModel('model/model.json');
+    model = await tf.loadLayersModel('model/model.json');
     console.log("done");
     document.getElementById("loader").style.display = "none";
 
